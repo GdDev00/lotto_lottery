691efc81055bc045f269302c990c67c99d793a44
GdDev00 <giacomo.dellomo00@gmail.com>
2020-11-23 22:31:49 +0100

    now print_utils automatic split too long line

--- lotto/print_utils.py
+++ lotto/print_utils.py
@@ -1,14 +1,14 @@
 #Custom module used for generate a visual representation of ticket
 
 class PrintUtils():
-    _MAX_TERMINAL_LINE_LENGHT = 35
+    _MAX_TERMINAL_LINE_LENGHT = 51
 
     #print a table horizontal line seperator
     #ex:
     #    "+-----------+"
     @staticmethod
     def print_horizontal_line_separator():
-        print("+{:-<33}+".format('-'))
+        print("+{:-<53}+".format('-'))
 
 
     #print a table row
@@ -37,20 +37,39 @@ class PrintUtils():
     #           2 = right
     @staticmethod
     def print_line(line, align = 0):
-        #align left
-        if align == 0:
-            print("+ {:31} +".format(line))        
-        #align center
-        elif align == 1:
-            print("+ {:^31} +".format(line))
+        sub_lines = []
+        words = line.split()
 
-        #align right
-        elif align == 2:
-            print("+ {:>31} +".format(line))
-        
-        #align parameter not valid
-        else:
-            raise ValueError("Align parameter is not valid!")
+        #for i in range(0,len(line), PrintUtils._MAX_TERMINAL_LINE_LENGHT):
+        #   s = ""
+        #   while True:
+        s = ""
+        for key, word in enumerate(words):
+            if len(s)  + len(word) + 1 < PrintUtils._MAX_TERMINAL_LINE_LENGHT:
+                s = s + word + " "
+            else:
+                sub_lines.append(s)
+                s = word + " "
+
+        sub_lines.append(s)
+
+
+
+        for element in sub_lines:
+            #align left
+            if align == 0:
+                print("+ {:51} +".format(element))        
+            #align center
+            elif align == 1:
+                print("+ {:^51} +".format(element))
+
+            #align right
+            elif align == 2:
+                print("+ {:>51} +".format(element))
+            
+            #align parameter not valid
+            else:
+                raise ValueError("Align parameter is not valid!")
 
     #print ticket header line
     @staticmethod
